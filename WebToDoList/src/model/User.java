b09510ad356862383f07a7988d083b1e71a188f3
elilueders <70550355+elilueders@users.noreply.github.com>
2020-10-07 22:22:50 -0500

    added class for User

---
+++ WebToDoList/src/model/User.java
@@ -0,0 +1,56 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "USER_ID")
+	private int id;
+	@Column(name = "USER_NAME")
+	private String userName;
+
+	public User() {
+		super();
+	}
+
+	public User(int id, String userName) {
+		super();
+		this.id = id;
+		this.userName = userName;
+	}
+
+	public User(String userName) {
+		super();
+		this.userName = userName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + "]";
+	}
+
+}
